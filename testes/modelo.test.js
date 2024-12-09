614d50baa5d8ab585dd858d7a12a9beba5b79aa8
Dtchaves <diogo.tuler@gmail.com>
2024-12-09 16:18:39 -0300

    Exercício feito!

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,51 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando recuperação de três perguntas', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  modelo.cadastrar_pergunta('2 + 2 = ?');
+  modelo.cadastrar_pergunta('3 + 3 = ?');
+  const perguntas = modelo.listar_perguntas(); 
+  expect(perguntas.length).toBe(3);
+  expect(modelo.get_pergunta(perguntas[0].id_pergunta).texto).toBe('1 + 1 = ?');
+  expect(modelo.get_pergunta(perguntas[1].id_pergunta).texto).toBe('2 + 2 = ?');
+  expect(modelo.get_pergunta(perguntas[2].id_pergunta).texto).toBe('3 + 3 = ?');
+});
+
+test('Testando cadastro de quatro respostas', () => {
+
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  modelo.cadastrar_pergunta('2 + 2 = ?');
+
+  const perguntas = modelo.listar_perguntas(); 
+  expect(perguntas.length).toBe(2);
+
+
+  modelo.cadastrar_resposta(perguntas[0].id_pergunta,'2');
+  modelo.cadastrar_resposta(perguntas[0].id_pergunta,'11');
+
+  modelo.cadastrar_resposta(perguntas[1].id_pergunta,'4');
+  modelo.cadastrar_resposta(perguntas[1].id_pergunta,'22');
+
+  expect(modelo.get_num_respostas(perguntas[0].id_pergunta)).toBe(2)
+  expect(modelo.get_num_respostas(perguntas[1].id_pergunta)).toBe(2)
+
+
+});
+
+test('Testando recuperação de duas respostas', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+
+  const perguntas = modelo.listar_perguntas(); 
+  expect(perguntas.length).toBe(1);
+
+
+  modelo.cadastrar_resposta(perguntas[0].id_pergunta,'2');
+  modelo.cadastrar_resposta(perguntas[0].id_pergunta,'11');
+
+  const respostas = modelo.get_respostas(perguntas[0].id_pergunta)
+
+  expect(respostas[0].texto).toBe('2')
+  expect(respostas[1].texto).toBe('11')
+});
